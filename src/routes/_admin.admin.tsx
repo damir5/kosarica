96e07193df579742e4e5ea968818cc61c155a308
damir <damir.bijuklic@gmail.com>
2026-01-14 10:02:54 +0000

    fix

--- src/routes/_admin.admin.tsx
+++ src/routes/_admin.admin.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
+import AdminHeader from '@/components/AdminHeader'
 
 export const Route = createFileRoute('/_admin/admin')({
   component: AdminLayout,
 })
 
 function AdminLayout() {
-  return <Outlet />
+  return (
+    <div className="min-h-screen bg-background">
+      <AdminHeader />
+      <Outlet />
+    </div>
+  )
 }
